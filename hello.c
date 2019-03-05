703676bcccd0bc1f9ccfb2581f6151af60d4d673
mrnguuyen <nnbrandon@att.net>
2019-03-04 17:44:46 -0800

    changes

--- hello.c
+++ hello.c
@@ -9,20 +9,53 @@
 */
 
 #define FUSE_USE_VERSION 26
+#define MAX_BLOCK_SIZE 4096		// 4KB block size
 
 #include <fuse.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <time.h>
 
-// 4 KB = 4096 Bytes block size
-#define BLOCK_SIZE 4096
 
+// Superblock struct
+typedef struct {
+    unsigned int magicNumber;   // 0xfa19283e
+    unsigned int totalNumBlocks;   // 256 blocks, 256 * 4KB = 1MB
+    unsigned int blockSize;     // 4096 BYTES or 4KB 
+} Superblock;
+
+// FileSystem struct
+typedef struct {
+    FILE *FS_FILE;  // Storage
+    Superblock sb;  // Superblock
+} FileSystem;
+
+
+// Initialize superblock at start up
+static void superblock_init(Superblock *sb, unsigned int totalNumBlocks, unsigned int blockSize) {
+    sb->magicNumber = 0xfa19283e;
+    sb->totalNumBlocks = totalNumBlocks;
+    sb->blockSize = blockSize;
+    printf("Initialized superblock with totalNumBlocks = %d and blockSize = %d\n", sb->totalNumBlocks, sb->blockSize);
+}
+
+// Initialize file system struct at start up
+static void filesys_init(FileSystem *filesystem, FILE *FS_FILE, unsigned int totalNumBlocks, unsigned int blockSize) {
+    printf("Initializing file system struct ... \n");
+    filesystem->FS_FILE = FS_FILE;
+    fseek(FS_FILE, 0, SEEK_SET);
+    superblock_init(&filesystem->sb, totalNumBlocks, blockSize);
+}
+
+static FileSystem fs;
 static const char *hello_str = "Hello World!\n";
 static const char *hello_path = "/hello";
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static int aofs_getattr(const char *path, struct stat *stbuf)
 {
 	// struct stat is the file's status
 	// struct stat {
@@ -40,7 +73,12 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 	// 	time_t    st_mtime;   /* time of last modification */
 	// 	time_t    st_ctime;   /* time of last status change */
 	// };
-
+	printf("aofs_getattr called\n");
+	printf("aofs_open: path = %s\n", path);
+	stbuf->st_uid = getuid(); // Owner of the file is user who mounted filesystem
+	stbuf->st_gid = getgid(); // Group of file
+	stbuf->st_atime = time(NULL); // Last access of file is right now
+	stbuf->st_mtime = time(NULL); // Last modification of file is right now
 	int res = 0;
 
 	memset(stbuf, 0, sizeof(struct stat));
@@ -56,14 +94,17 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 		printf("Path is /hello\n");
 	} else
 		// -ENOENT Path doesn't exist
-		res = -ENOENT;
+		stbuf->st_mode = S_IFREG | 0644;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = 1024;
+		// res = -ENOENT;
 
 	return res;
 }
 
 
 // I don't think this will be needed
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+static int aofs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
 	(void) offset;
@@ -80,8 +121,9 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+static int aofs_open(const char *path, struct fuse_file_info *fi)
 {
+	printf("aofs_open: path = %s\n", path);
 	// -ENOENT Path doesn't exist
 	if (strcmp(path, hello_path) != 0)
 		return -ENOENT;
@@ -93,9 +135,10 @@ static int hello_open(const char *path, struct fuse_file_info *fi)
 	return 0;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+static int aofs_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
+	printf("aofs_read: path = %s\n", path);
 	size_t len;
 	(void) fi;
 	// -ENOENT Path doesn't exist
@@ -113,14 +156,66 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 	return size;
 }
 
-static struct fuse_operations hello_oper = {
-	.getattr	= hello_getattr,
-	.readdir	= hello_readdir,
-	.open		= hello_open,
-	.read		= hello_read,
+// NOT WORKING
+static int aofs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+	printf("create function has been called\n");
+	int res;
+	res = open(path, fi->flags, mode);
+	if(res == -1) {
+		return -errno;
+	}
+	fi->fh = res;
+	return 0;
+}
+
+// NOT WORKING
+static int aofs_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	printf("mknod function has been called!\n");
+	printf("path = %s\n", path);
+	int res;
+	if(S_ISREG(mode)) {
+		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if(res >= 0) {
+			res = close(res);
+		}
+	}
+	else if(S_ISFIFO(mode)) {
+		res = mkfifo(path, mode);
+	}
+	else {
+		res = mknod(path, mode, rdev);
+	}
+
+	if(res == -1) {
+		perror("mknod error!\n");
+		exit(0);
+	}
+	
+	return 0;
+}
+
+static struct fuse_operations aofs_oper = {
+	.getattr	= aofs_getattr,
+	.readdir	= aofs_readdir,
+	.open		= aofs_open,
+	.read		= aofs_read,
+	.create		= aofs_create,
+	.mknod		= aofs_mknod,
 };
 
 int main(int argc, char *argv[])
 {
-	return fuse_main(argc, argv, &hello_oper, NULL);
+	FILE *FS_FILE = fopen("FS_FILE", "r+");
+	if(FS_FILE == NULL) {
+		FS_FILE = fopen("FS_FILE", "wb+");
+		printf("FS_FILE has been created\n");
+		filesys_init(&fs, FS_FILE, 256, MAX_BLOCK_SIZE);
+	}
+	else {
+		printf("FS_FILE is not NULL!\n");
+	}
+
+	return fuse_main(argc, argv, &aofs_oper, NULL);
 }
--- superblock.h
+++
@@ -1,7 +0,0 @@
-#ifndef SUPERBLOCK_H
-#define SUPERBLOCK_H
-
-typedef struct {
-    unsigned int magicNumber;
-    // Bitmap goes here
-} Superblock;
